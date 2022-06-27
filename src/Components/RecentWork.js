cb96f671dd2119446792c2933995d79a42c8eae9
Daniel <daniel-mcintyre@hotmail.com>
2022-06-27 19:39:01 -0400

    added to my work section details

--- src/Components/RecentWork.js
+++ src/Components/RecentWork.js
@@ -2,6 +2,7 @@ import Slider from 'react-slick';
 import workpic1 from '../assets/Video Diary App Example.png';
 import workpic2 from '../assets/gihpy-project-example.png';
 import workpic3 from '../assets/movie-mood-example.png';
+import { SiCss3, SiJavascript, SiSass, SiReact, SiFirebase } from 'react-icons/si'
 
 function RecentWork() {
 
@@ -26,26 +27,58 @@ return (
                         <div className="work-example-pic">
                             <a href="https://eager-curran-c5f5e6.netlify.app/"><img src={workpic1} alt="" /></a>
                              <div className="work-example">
-                            <h3>Video Diary</h3>
+                            <h1>Video Diary</h1>
                             <p>An application built in React, Video Diary uses Google's YouTube API and firebase. Fetches and displays the current trending videos on YouTube. Users can also search for thier desired video and also save them to view later or delete from the page. </p>
+                              <div className="made-with">
+                                    <h3>Made With</h3>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                    </div>
                         </div>
-                            </div>
+                        </div>
                     </div> 
                         <div className="work-example-one-container">
                         <div className="work-example-pic">
                             <a href="https://eager-curran-c5f5e6.netlify.app/"><img src={workpic2} alt="" /></a>
                              <div className="work-example">
-                            <h3>Giphy Sentiment</h3>
+                            <h1>Giphy Sentiment</h1>
                             <p>An application built using HTML, CSS, SASS, JavaScript and the MovieDB REST API, MovieMood fetches movies the user can watch based on thier input of genre and time period for the last 100 years. Made in collaboration with friend Kimberly Schubert.</p>
-                        </div>
+                            <div className="made-with">
+                                    <h3>Made With</h3>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                    </div>
                             </div>
+                             </div>
                     </div> 
                     <div className="work-example-three-container">
                         <div className="work-example-pic">
                             <a href="https://eager-curran-c5f5e6.netlify.app/"><img src={workpic3} alt="" /></a>
                              <div className="work-example">
-                            <h3>Movie Mood</h3>
-                            <p>An application built using HTML, CSS, SASS, JavaScript and the MovieDB REST API, MovieMood fetches movies the user can watch based on thier input of genre and time period for the last 100 years. Made in collaboration with friend Kimberly Schubert.</p>
+                            <h1>Movie Mood</h1>
+                                <p>An application built using HTML, CSS, SASS, JavaScript and the MovieDB REST API, MovieMood fetches movies the user can watch based on thier input of genre and time period for the last 100 years. Made in collaboration with friend Kimberly Schubert.</p>
+                                <div className="made-with">
+                                    <h3>Made With</h3>
+                                    <div className="made-with-icons">
+                                        <ul>
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                        </ul>
+                                    </div>
+                                    </div>
                         </div>
                             </div>
                     </div>                
--- src/Components/TestimonalsSection.js
+++ src/Components/TestimonalsSection.js
@@ -1,6 +1,7 @@
 import Slider from 'react-slick';
 import headshot1 from '../assets/bryan-pic.jpg';
 
+
 function TestimonalsSection() {
 
       const settings = {
@@ -20,12 +21,12 @@ function TestimonalsSection() {
             <Slider {...settings}>
                 <div className="testimonals-one-container">
             <h1>"</h1>
-            <p>Daniel is a pleasure to work with! He's reliable, dependable and an asset for on any team! - Kevin Zhang, Front-End Web Developer</p>
-                    <img src="" alt="" />
+            <p>Daniel has been great to work with. In the short time that we have worked in a professional setting, Daniel has been an active participant in the non-technical conversations and has taken charge of the technical aspects of our project. He is knowledgeable about his field and is able to effectively work in a team setting and complete deliverables on time. - Bryan Jok, Product Manager, Co.Lab </p>
+                    <img src={headshot1} alt="" />
                 </div>
                 <div className="testimonals-two-container">
                     <h1>"</h1>
-            <p>Daniel has been great working with so far. In the short time that we have worked in a professional setting, Daniel has been an active participant in the non-technical conversations and has taken charge of the technical aspects of our project. He is knowledgeable about his field and is able to effectively work in a team setting and complete deliverables on time. - Bryan Jok, Product Manager, Co.Lab </p>
+            <p>Daniel is a pleasure to work with! He's reliable, dependable and an asset for on any team! - Kevin Zhang, Front-End Web Developer</p>
                     <img src={headshot1} alt="" />
                 </div>
             </Slider>
