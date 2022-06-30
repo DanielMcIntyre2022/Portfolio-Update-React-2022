8747a9535155346e4cd7bb6131cfae798649fdb4
Daniel <daniel-mcintyre@hotmail.com>
2022-06-30 15:07:54 -0400

    finalized more testimonals

--- src/Components/TestimonalsSection.js
+++ src/Components/TestimonalsSection.js
@@ -1,6 +1,7 @@
 import Slider from 'react-slick';
 import headshot1 from '../assets/bryan-pic.jpg';
 import headshot2 from '../assets/koki-headshot.png';
+import headshot3 from '../assets/alvin-headshot.jpg';
 import unknownheadshot from '../assets/unknown-person.jpg';
 
 function TestimonalsSection() {
@@ -12,7 +13,7 @@ function TestimonalsSection() {
       slidesToScroll: 1,
         autoplay: true,
         speed: 2000,
-      autoplaySpeed: 9000,
+      autoplaySpeed: 6000,
 
     };
 
@@ -28,7 +29,11 @@ function TestimonalsSection() {
             <p>"Daniel's eagerness to learn and his commitment to writing better, more efficient code, paired with his welcoming and positive attitude, make him a very valuable asset." - <a href="https://www.linkedin.com/in/koki-vasileski/" target="_blank" rel="noreferrer">Koki Vasileski, Front-End Developer</a></p>
                     <img src={headshot2} alt="" />
                 </div>
-                <div className="testimonals-two-container">
+                 <div className="testimonals-three-container">
+            <p>"Daniel is a forward thinking, intelligent, and a motivated frontend developer who excels in understanding new concepts. He is not afraid to ask for help when he needs it nor does he refuse to help when you ask." - <a href="https://www.linkedin.com/in/alvinwchou/" target="_blank" rel="noreferrer">Alvin Chou, Front-End Developer</a></p>
+                    <img src={headshot3} alt="" />
+                </div>
+                <div className="testimonals-four-container">
             <p>"Daniel is a pleasure to work with! He's reliable, dependable and an asset for on any team!" - <a href="https://www.linkedin.com/in/kevzhang-dev/" target="_blank" rel="noreferrer">Kevin Zhang, Application Developer</a></p>
                     <img src={unknownheadshot} alt="" />
                 </div>
