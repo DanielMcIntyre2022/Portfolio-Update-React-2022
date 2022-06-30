3d4a782a2bc4eb41ccb7973964a10fe02cd4e651
Daniel <daniel-mcintyre@hotmail.com>
2022-06-30 14:56:41 -0400

    added more testimonals

--- src/Components/TestimonalsSection.js
+++ src/Components/TestimonalsSection.js
@@ -1,5 +1,6 @@
 import Slider from 'react-slick';
 import headshot1 from '../assets/bryan-pic.jpg';
+import headshot2 from '../assets/koki-headshot.png';
 import unknownheadshot from '../assets/unknown-person.jpg';
 
 function TestimonalsSection() {
@@ -22,6 +23,10 @@ function TestimonalsSection() {
                 <div className="testimonals-one-container">
             <p>"Daniel is great to work with. He is an active participant in non-technical conversations and has taken charge of technical projects. He is knowledgeable about his field and is able to effectively work in a team" - <a href="https://www.linkedin.com/in/bryan-jok-ab87a497/" target="_blank" rel="noreferrer">Bryan Jok, Product Manager, Co.Lab</a> </p>
                     <img src={headshot1} alt="" />
+                </div>
+                 <div className="testimonals-three-container">
+            <p>"Daniel's eagerness to learn and his commitment to writing better, more efficient code, paired with his welcoming and positive attitude, make him a very valuable asset." - <a href="https://www.linkedin.com/in/koki-vasileski/" target="_blank" rel="noreferrer">Koki Vasileski, Front-End Developer</a></p>
+                    <img src={headshot2} alt="" />
                 </div>
                 <div className="testimonals-two-container">
             <p>"Daniel is a pleasure to work with! He's reliable, dependable and an asset for on any team!" - <a href="https://www.linkedin.com/in/kevzhang-dev/" target="_blank" rel="noreferrer">Kevin Zhang, Application Developer</a></p>
