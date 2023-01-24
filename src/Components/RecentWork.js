203f2b41ee3d377434bdf1c97c5f0fb0d35af1c7
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-24 16:14:01 -0500

    updated website link for e-comm work example

--- src/Components/RecentWork.js
+++ src/Components/RecentWork.js
@@ -39,7 +39,7 @@ function RecentWork(props) {
                     <Slider {...settings}>
                     <div className="work-example-container">
                             <div className="work-example-pic">
-                                <a href="https://soicalite.netlify.app/" target="_blank" rel="noreferrer"><img src={giphy5} alt="" /></a>
+                                <a href="https://dans-e-commerce.up.railway.app/" target="_blank" rel="noreferrer"><img src={giphy5} alt="" /></a>
                                 <div className="work-example">
                                     <h1><a href="https://soicalite.netlify.app/" target="_blank" rel="noreferrer">E-Commerce Web Application</a></h1>
                                     <p>A full-stack MongoDB, Express, React, Node (MERN) E-Commerce Web Application. Made with TailwindCSS and Material UI CSS libraries. Utilizes Redux toolkit for state mangement and JSON web tokens (JWT) for user authentication and authorization.</p>
