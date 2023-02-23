3f4249283519d04312cb6c1e81f257fc109775d3
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-23 13:12:03 -0500

    additional updates to resume

--- src/Components/AboutMeSection.js
+++ src/Components/AboutMeSection.js
@@ -1,5 +1,5 @@
 import headshot from '../assets/daniel-mcintyre-headshot.jpg';
-import resume from '../assets/Daniel_McIntyre_2023_Final_Resume.pdf';
+import resume from '../assets/DanielMcIntyre_2023_Resume.pdf';
 import { useEffect, useRef } from 'react';
 
 function AboutMeSection(props) {
