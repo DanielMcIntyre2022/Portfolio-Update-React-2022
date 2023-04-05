d52c1859308e406f34da8391e3749df4f9c20f7c
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-04-05 18:14:39 -0400

    updates to socials component

--- src/Components/SocialsComponent.js
+++ src/Components/SocialsComponent.js
@@ -8,7 +8,6 @@ function SocialsComponent() {
                 <div className="socials-list">
             <a href="https://github.com/DanielMcIntyre2022" target="_blank" rel="noreferrer"><FaGithub/></a>
             <a href="https://www.linkedin.com/in/daniel-mcintyre-2404a827/" target="_blank" rel="noreferrer"><FaLinkedin/></a>
-            <a href="https://twitter.com/DanielMcIntyree" target="_blank" rel="noreferrer"><FaTwitter/></a>
             <a href="https://medium.com/@daniel-mcintyre" target="_blank" rel="noreferrer"><FaMedium/></a>
                 </div>
             </div>
